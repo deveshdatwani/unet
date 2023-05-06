42f002f86b4d677ad1cfe3d8ce9407069f41f4f4
deveshdatwani <datwanidevesh@gmail.com>
2023-05-06 14:50:18 -0400

    double conv

--- unet/model.py
+++ unet/model.py
@@ -6,27 +6,32 @@ from torchvision.transforms import ToTensor, Resize, InterpolationMode, Normaliz
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DoubleConv, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-        )
+        self.conv = nn.Sequential(nn.Conv2d(in_channels, out_channels, padding=1, kernel_size=3),
+                                                   nn.BatchNorm2d(out_channels),
+                                                   nn.ReLU(),
+                                                   nn.Conv2d(out_channels, out_channels, padding=1, kernel_size=3),
+                                                   nn.BatchNorm2d(out_channels),
+                                                   nn.ReLU())
+
 
     def forward(self, x):
         return self.conv(x)
 
+
 class UNet(nn.Module):
-    def __init__(
-            self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
-            super(UNet, self).__init__()
-            self.double_convolution = nn.Sequential(nn.Conv2d(in_channels, out_channels, padding=1, kernel_size=3),
-                                                   nn.BatchNorm2d(out_channels),
-                                                   nn.ReLU(),
-                                                   nn.Conv2d(out_channels, out_channels),
-                                                   nn.BatchNorm2d(out_channels),
-                                                   nn.ReLU())
-            self.bottle_neck = None
+    def __init__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
+        super(UNet, self).__init__()
+        self.double_convolution = DoubleConv(3, 64)
+        self.bottle_neck = None
+
+
+    def forward(self, x):
+        x = self.double_convolution(x)
+        return x
+
+
+if __name__ == '__main__':
+    model  = UNet()
+    input = torch.rand(size=(3, 572, 572))
+    print(model(input.unsqueeze(0)).shape) 
             
