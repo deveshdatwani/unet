3f6a74aa2fd99ef644998aee9a4c09eaa2c6cf3b
deveshdatwani <datwanidevesh@gmail.com>
2023-05-06 21:21:43 -0400

    how do we downsample channel size up 2x2

--- unet/model.py
+++ unet/model.py
@@ -7,11 +7,11 @@ class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DoubleConv, self).__init__()
         self.conv = nn.Sequential(nn.Conv2d(in_channels, out_channels, padding=1, kernel_size=3),
-                                                   nn.BatchNorm2d(out_channels),
-                                                   nn.ReLU(),
-                                                   nn.Conv2d(out_channels, out_channels, padding=1, kernel_size=3),
-                                                   nn.BatchNorm2d(out_channels),
-                                                   nn.ReLU())
+                                    nn.BatchNorm2d(out_channels),
+                                    nn.ReLU(inplace=True),
+                                    nn.Conv2d(out_channels, out_channels, padding=1, kernel_size=3),
+                                    nn.BatchNorm2d(out_channels),
+                                    nn.ReLU(inplace=True))
 
 
     def forward(self, x):
@@ -21,12 +21,34 @@ class DoubleConv(nn.Module):
 class UNet(nn.Module):
     def __init__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
         super(UNet, self).__init__()
-        self.double_convolution = DoubleConv(3, 64)
+        self.downsample = nn.MaxPool2d(2)
+        self.double_convolution_1 = DoubleConv(3, 64)
+        self.double_convolution_2 = DoubleConv(64, 128)
+        self.double_convolution_3 = DoubleConv(128, 256)
+        self.double_convolution_4 = DoubleConv(256, 512)
+        self.double_convolution_5 = DoubleConv(512, 1024)
+        self.upsample_5 = nn.Upsample((71, 71, 512))
+        self.up_double_convolution_5 = DoubleConv(1024, 512)
         self.bottle_neck = None
-
+        
 
     def forward(self, x):
-        x = self.double_convolution(x)
+        x_skip_connection_1 = self.double_convolution_1(x)
+        x = self.downsample(x_skip_connection_1)
+        x_skip_connection_2 = self.double_convolution_2(x)
+        x = self.downsample(x_skip_connection_2)
+        x_skip_connection_3 = self.double_convolution_3(x)
+        x = self.downsample(x_skip_connection_3)
+        x_skip_connection_4 = self.double_convolution_4(x)
+        x = self.downsample(x_skip_connection_4)
+        x = self.double_convolution_5(x)
+        x = self.upsample_5(x)
+        print(f'AFTER UPSAMPLE {x.shape}')
+        print(x_skip_connection_4.shape)
+        x = torch.cat((x, x_skip_connection_4))
+        x = self.up_double_convolution_5(x)
+
+
         return x
 
 
