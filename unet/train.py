bf8cc8f094009e92f9ca547e51d5514fc75bb241
deveshdatwani <datwanidevesh@gmail.com>
2023-05-11 00:29:28 -0400

    interim

--- unet/train.py
+++ unet/train.py
@@ -70,6 +70,7 @@ class Trainer(object):
 
 if __name__ == "__main__":
     torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(torch.device())
     EPOCHS = 1
     BATCH_SIZE = 1
     PATH = '/home/deveshdatwani/Datasets/Caravan'
@@ -89,7 +90,7 @@ if __name__ == "__main__":
     else:
         MODEL_WEIGHTS_ADDRESS = '/home/deveshdatwani/Unet/weights/model.pt'
         if 'model.pt' in os.listdir(MODEL_WEIGHTS_DIR):
-            checkpoint = torch.load(MODEL_WEIGHTS_ADDRESS)
+            checkpoint = torch.load(MODEL_WEIGHTS_ADDRESS, map_location=torch.device())
             model.load_state_dict(checkpoint['model_state_dict'])
 
         else:
