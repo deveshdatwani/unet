e32b62291b5497fbae765682dcb5324380fed277
deveshdatwani <datwanidevesh@gmail.com>
2023-05-19 15:51:38 -0400

    inference module for quick unit test

---
+++ unet/inference.py
@@ -0,0 +1,43 @@
+import os
+import torch
+import argparse
+from model import UNet
+from utils import infer
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str)
+    parser.add_argument('--image', type=int)
+    args = parser.parse_args()
+
+
+    torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    EPOCHS = 1
+    BATCH_SIZE = 1
+    DATA_PATH = '/home/deveshdatwani/Datasets/CARAVANA'
+    MODEL_TYPE = 'attention'
+    model = UNet()
+    MODEL_WEIGHTS_DIR = '/home/deveshdatwani/Unet/weights'
+
+    if MODEL_TYPE == 'attention':
+        MODEL_WEIGHTS_ADDRESS = '/home/deveshdatwani/Unet/weights/attention-model.pt'
+        if 'attention-model.pt' in os.listdir(MODEL_WEIGHTS_DIR):
+            checkpoint = torch.load(MODEL_WEIGHTS_ADDRESS, map_location=torch.device('cpu'))
+            model.load_state_dict(checkpoint['model_state_dict'])
+        
+        else:
+            print('NO CHECKPOINTS FOUND')
+        
+    else:
+        MODEL_WEIGHTS_ADDRESS = '/home/deveshdatwani/Unet/weights/model.pt'
+        if 'model.pt' in os.listdir(MODEL_WEIGHTS_DIR):
+            checkpoint = torch.load(MODEL_WEIGHTS_ADDRESS, map_location=torch.device('cpu'))
+            model.load_state_dict(checkpoint['model_state_dict'])
+
+        else:
+            print('NO CHECKPOINTS FOUND')
+
+    images_address = os.path.join(DATA_PATH, os.listdir(DATA_PATH)[args.image])
+    infer(images_address, model)
---
+++ unet/utils.py
@@ -0,0 +1,23 @@
+import cv2
+import torch 
+import numpy as np 
+from time import time
+from PIL import Image
+from matplotlib import pyplot as plt
+from torchvision.transforms import ToTensor, Resize, InterpolationMode, Normalize
+
+
+def infer(image_address, model):
+    image_size = [572, 572]
+    image = np.array(cv2.imread(image_address), dtype=np.float32) / 255.0
+    image = image.transpose((2,0,1)) 
+    image = torch.from_numpy(image)
+    image = Resize(image_size, interpolation=InterpolationMode.BILINEAR, antialias=True )(image)
+    start = time()
+    inference = model(image.unsqueeze(0))
+    end = time()
+    print(f'total time {end-start}')
+    plt.imshow(inference[0].detach().numpy().transpose(1,2,0))
+    plt.show()
+
+    return None
